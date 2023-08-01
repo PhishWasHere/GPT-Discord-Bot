414e492ed93dc830e1a3cf27c5fc19db33a582b2
PhishWasHere <121412190+PhishWasHere@users.noreply.github.com>
2023-08-01 20:09:03 +1000

    responds to DM

--- server/config/index.js
+++ server/config/index.js
@@ -1,4 +1,4 @@
-const { GatewayIntentBits, Client } = require ('discord.js');
+const { GatewayIntentBits, Client, Partials } = require ('discord.js');
 
 const client = new Client({
     intents: [
@@ -7,10 +7,19 @@ const client = new Client({
         GatewayIntentBits.MessageContent,
         GatewayIntentBits.GuildMessageReactions,
         GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.DirectMessages
+    ],
+    partials: [
+        Partials.Channel,
+        Partials.Message
     ]
 });
 
-client.on('messageCreate', async (message) => {
+client.on('message', async (message) => { // for DM's
+    console.log(message);
+});
+
+client.on('messageCreate', async (message) => { // for guild/server messages
     console.log(message.content);
 
     if (!message?.author.bot) {
