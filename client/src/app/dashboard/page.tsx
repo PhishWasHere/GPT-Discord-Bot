61217758bc48a36eb280d48b6e6e38cee713a9c8
PhishWasHere <121412190+PhishWasHere@users.noreply.github.com>
2023-08-18 19:11:26 +1000

    reduced code needed for nav

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -1,17 +1,44 @@
 'use client'
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import Link from 'next/link';
 
 import { useAppSelector } from '@/utils/redux/store';
 
 export default function Dashboard() {
-  const userdata = useAppSelector((state) => state.authReducer.value);
-  console.log(userdata);
-  
+  const userData = useAppSelector((state) => state.authReducer.value);
+  console.log(userData);
     
+  if(!userData || userData.isAuth === false){
+    return(
+      <>  
+        <main className="relative py-16 bg-gradient-to-br from-sky-50 to-gray-200 w-full ">
+            <div className="relative container m-auto px-6 text-gray-500 md:px-12 xl:px-40">
+                <div className="m-auto md:w-8/12 lg:w-6/12 xl:w-6/12">
+                    <div className="rounded-xl bg-white shadow-xl">
+                        <section className="p-6 sm:p-16">
+                            <div className="space-y-4">
+                                <h2 className="mb-8 text-2xl text-cyan-900 font-bold text-center">Login to view this page</h2>
+                            </div>
+
+                            <Link href='/login' className="mt-16 grid space-y-4 group">
+                                <button className="relative flex items-center space-x-4 justify-center h-12 px-6 border-2 border-gray-300 rounded-full transition duration-300 hover:border-indigo-400 focus:bg-blue-50 active:bg-blue-100">
+                                  <span className="block w-max font-semibold tracking-wide text-gray-700 text-sm transition duration-300 group-hover:text-blue-600 sm:text-base">Login</span>
+                                </button>
+                            </Link>
+                        </section>
+                    </div>
+                </div>
+            </div>
+        </main>
+      </>
+    )
+  }
+
   return (
-    <div>
-      {userdata.username}
-    </div>
+    <main className='text-gray-900'>
+      {userData.username}
+      
+    </main>
   );
 }
--- client/src/components/nav/index.tsx
+++ client/src/components/nav/index.tsx
@@ -107,20 +107,6 @@ export default function NavBar () {
                         </li>
                     ))}
                 </ul>
-
-                {!userData ? (
-                    <Link href='/login' className="flex items-center justify-center w-full h-16 mt-auto bg-gray-800 hover:bg-gray-700 hover:text-gray-300">
-                        <svg className="w-6 h-6 stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"> 
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="" /> 
-                        </svg>
-                    </Link>
-                ) : (
-                    <button onClick={() => handleLogout()} className="flex items-center justify-center w-full h-16 mt-auto bg-gray-800 hover:bg-gray-700 hover:text-gray-300">
-                        <svg className="w-6 h-6 stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="" /> 
-                        </svg>
-                    </button>
-                )}
             </div>
 
            <div className={`md:flex flex-col items-center w-48 h-full overflow-hidden text-gray-100 bg-gray-900 rounded-r ${showComponent ? '' : 'hidden'}`}>
@@ -128,7 +114,7 @@ export default function NavBar () {
                     {userData ? (
                         <>
                         <div className='w-8 h-8 fill-current'>
-                            <p className='ml-2 text-xl font-bold'>{userData.username}</p>
+                            <p className='ml-2 text-xl font-bold'><Link href='/'>{userData.username}</Link></p>
                         </div>
                         </>
                     ) : (
@@ -136,7 +122,7 @@ export default function NavBar () {
                         <svg className="w-8 h-8 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                             <path d="M11 17a1 1 0 001.447.894l4-2A1 1 0 0017 15V9.236a1 1 0 00-1.447-.894l-4 2a1 1 0 00-.553.894V17zM15.211 6.276a1 1 0 000-1.788l-4.764-2.382a1 1 0 00-.894 0L4.789 4.488a1 1 0 000 1.788l4.764 2.382a1 1 0 00.894 0l4.764-2.382zM4.447 8.342A1 1 0 003 9.236V15a1 1 0 00.553.894l4 2A1 1 0 009 17v-5.764a1 1 0 00-.553-.894l-4-2z" />
                         </svg>
-                        <span className="ml-2 text-sm font-bold">Bully Me GPT</span>
+                        <span className="ml-2 text-sm font-bold"><Link href='/'>Bully Me GPT</Link></span>
                         </>
                     )}
                 </section>
