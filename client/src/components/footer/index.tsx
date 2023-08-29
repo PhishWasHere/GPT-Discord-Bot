a5bc3cb7dd374c01e1912715707feed71f3e4ab0
PhishWasHere <121412190+PhishWasHere@users.noreply.github.com>
2023-08-29 19:30:05 +1000

    seperated api server

--- client/src/components/footer/index.tsx
+++
@@ -1,9 +0,0 @@
-
-
-export default function Footer () {
-    return(
-        <footer className="bg-gradient-to-r h-2 from-blue-700 to-violet-400 mt-auto" aria-label="footer">
-            <span className=""></span>
-        </footer>
-    )
-}
